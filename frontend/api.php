b77c8b3a4a408cdd787b55a3cffc078e0a5f74ac
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-02-20 10:12:54 +0100

    Add a function to get the available languages

--- frontend/api.php
+++ frontend/api.php
@@ -5,3 +5,12 @@
 function supplang_switcher() {
 	include 'templates/language-selector.php';
 }
+
+/**
+ * Returns an array of available languages. Each item is an array with the following properties:
+ * * `name` - The name of the language, in the language itself
+ * * `locale` - The name of the WordPress locale for this language
+ */
+function supplang_languages() {
+  return SUPPLANG_LANGUAGES;
+}
