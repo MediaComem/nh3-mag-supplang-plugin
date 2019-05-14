3151187e88a2b9def8778c4afb1386cce1bc2508
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-05-14 16:57:14 +0200

    Add the Edition label

--- frontend/templates/language-selector.php
+++ frontend/templates/language-selector.php
@@ -4,7 +4,7 @@
 
 	<select name="supplang-uil" id="supplang-selector-select">
 	<?php foreach ( supplang_languages() as $language ) : ?>
-	<option value="<?php echo esc_html( $language['slug'] ); ?>" <?php esc_html_e('Edition', 'supplang'); ?> <?php echo get_locale() === $language['locale'] ? 'selected' : ''; ?>><?php echo esc_html( $language['name'] ); ?></option>
+	<option value="<?php echo esc_html( $language['slug'] ); ?>" <?php echo get_locale() === $language['locale'] ? 'selected' : ''; ?>><?php esc_html_e( 'Edition', 'supplang' ); ?> <?php echo esc_html( $language['name'] ); ?></option>
 	<?php endforeach ?>
 	</select>
 
