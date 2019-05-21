db4866f2cc7a9a28a6a09045d969cc024dfe44f2
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-05-21 14:34:41 +0200

    Fix bug where the page was not reloading

--- frontend/js/language-switcher.js
+++ frontend/js/language-switcher.js
@@ -3,7 +3,9 @@
  * while setting the new locale to the selected language.
  */
 (function($) {
+  console.log('Supplang Switcher Loaded');
   $('#supplang-selector-select').on('change', function() {
+    console.log('Supplang Switcher Changed');
     var newUrl;
     var currentUrl = window.location.href;
     // Case when the supplang get param is already set in the current URL
@@ -16,6 +18,6 @@
     } else {
       newUrl = currentUrl + "?uil=" + this.value;
     }
-    window.location = newUrl;
+    window.location.href = newUrl;
   });
 })(jQuery)
