a9f21cff009a3eac84bbcf8419fbe8b161e8dc5a
Mathias Oberson <mathias.oberson@heig-vd.ch>
2019-06-04 12:50:28 +0200

    Release new patch version - v2.4.4

--- supplang.php
+++ supplang.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Supplang
  * Description: Flexible language manager that allows for a complete separation between UI language and content language.
- * Version:     2.4.3
+ * Version:     2.4.4
  * Author:      Mathias Oberson - MEI
  * Author URI:  https://heig-vd.ch/rad/instituts/mei/
  * Text Domain: supplang
